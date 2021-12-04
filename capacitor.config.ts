d2d9946cc43b55e771caa92194516645261c106b
Igor <igorsiriani@gmail.com>
2021-12-04 10:32:10 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'social-teacher',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
