4311fab2e637a6fee3021f014398d1495647551d
Igor <igorsiriani@gmail.com>
2021-12-04 14:07:47 -0300

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  postList = [
+    {
+      user: {
+        first_name: 'Jon',
+        last_name: 'Doe'
+      },
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id magna sit amet',
+      comments: [
+        {
+          user: {
+            first_name: 'Jon',
+            last_name: 'Doe'
+          },
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus hendrerit metus, ut ullamcorper'
+        },
+        {
+          user: {
+            first_name: 'Jon',
+            last_name: 'Doe'
+          },
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+      ]
+    },
+    {
+      user: {
+        first_name: 'Amanda',
+        last_name: 'Silva'
+      },
+      post: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus hendrerit metus, ut ullamcorper quam finibus at. Etiam id magna sit amet',
+      comments: [
+        {
+          user: {
+            first_name: 'Jon',
+            last_name: 'Doe'
+          },
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus hendrerit metus, ut ullamcorper'
+        },
+        {
+          user: {
+            first_name: 'Zé',
+            last_name: 'Carlos'
+          },
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        }
+      ]
+    },
+  ]
+
   constructor() {}
 
 }
